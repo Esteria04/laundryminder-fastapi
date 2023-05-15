33bb01c2d8d2ab4c0ed3efb39fe5f26183b5f5eb
Esteria04 <dev.esteria04@gmail.com>
2023-05-15 15:25:15 +0900

    .

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+import databases
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+user, password, host, port, database = (
+    os.environ.get('user'),
+    os.environ.get('password'),
+    os.environ.get('host'),
+    os.environ.get('port'),
+    os.environ.get('database')
+)
+
+# Create a database URL
+DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+# Create a database object
+database = databases.Database(DATABASE_URL)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@app.get("/dormitory")
+async def get_dormitory(dorm_id: int):
+    query = "SELECT * FROM dormitory WHERE id = :dorm_id"
+    dormitory = await database.fetch_one(query=query, values={"dorm_id": dorm_id})
+    return dormitory
+
+@app.get("/machine")
+async def get_machines(dorm_id: int,type: str):
+    if type == "w":
+        typeText = "washing_machine"
+    elif type == "d":
+        typeText = "drying_machine"
+    else:
+        return {"Error":"unexpected type"}
+    query = f"SELECT * FROM {typeText} WHERE dormitory_id = :dorm_id"
+    machines = await database.fetch_all(query=query, values={"dorm_id": dorm_id})
+    print(query)
+    return machines
+
+# @app.get("/cycle")
+# async def get_cycles(dorm_id: int,type: str,):
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app)
